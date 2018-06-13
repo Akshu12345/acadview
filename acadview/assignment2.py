781a262011c2d130ed7ba5b43c205672a649d051
Akshu12345 <akshitasharma01111@gmail.com>
2018-06-13 17:09:23 +0530

    Assignment2 completed

---
+++ acadview/assignment2.py
@@ -0,0 +1,33 @@
+# Ques1
+print("My name is akshita")
+
+
+# Ques2
+a="hello"
+b="word"
+print(a+b)
+
+
+# Ques3
+x="python"
+y=11.3
+z=40
+print(x,y,z,sep=',')
+
+
+# Ques4
+print('Let\'s get started')
+
+
+# Ques5
+s="Acadview"
+course="Python"
+fees=5000
+print("training from %s, course is %s and fees is %d"%(s,course,fees))
+
+
+# Ques6
+name="Tony stark"
+salary=1000000
+print("%s %d"%(name,salary))
+
